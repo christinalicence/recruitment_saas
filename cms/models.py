b8b50985e1c663519d79851010a3edf186250993
christinalicence <christinalicence@gmail.com>
2026-01-14 10:48:20 +0000

    write test to ensure that data is kept seperate from other tenants

--- cms/models.py
+++ cms/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 
 class Page(models.Model):
+    # Basic CMS Page model
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-
     def __str__(self):
         return self.title
+
+
+class Job(models.Model):
+    # Job Posting model
+    title = models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150, blank=True, null=True) # Optional
+    salary = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    summary = models.CharField(max_length=255, help_text="A one-sentence hook for the job list.")
+    description = models.TextField() # Ad description
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.title} - {self.location}"
+    
