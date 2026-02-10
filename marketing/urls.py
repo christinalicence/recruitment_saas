6407dd7cb14727774b5587983c8a23c48fff29c5
christinalicence <christinalicence@gmail.com>
2026-02-10 14:16:41 +0000

    update about page url in marketing urls

--- marketing/urls.py
+++ marketing/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path("choose-template/", views.template_select, name="template_select"),
     path("preview/<str:template_id>/", views.template_preview, name="template_preview"),
     path("signup/", views.tenant_signup, name="tenant_signup"),
-    path("about/", views.about_page, name="about"),
-]
+    path("about/", views.company_about, name="about")
